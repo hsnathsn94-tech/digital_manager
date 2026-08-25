8dba04d5fd5ad2dac2fe7f677c4dc138f3240511
Sakhr Al-Rashidi <hsnathsn94@gmail.com>
2026-08-26 01:50:44 +0300

    Switch DatabaseHelper to In-Memory for Web/Desktop compatibility

--- lib/core/services/database_helper.dart
+++ lib/core/services/database_helper.dart
@@ -1,53 +1,40 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 import '../models/product_model.dart';
+import '../models/customer_model.dart';
+import '../models/order_model.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
-  static Database? _database;
-
   DatabaseHelper._init();
 
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDB('digital_ceo.db');
-    return _database!;
+  // مصفوفات محلية لتخزين البيانات مؤقتاً في الذاكرة
+  final List<ProductModel> _products = [];
+  final List<CustomerModel> _customers = [];
+  final List<OrderModel> _orders = [];
+
+  // المنتجات
+  Future<void> insertProduct(ProductModel product) async {
+    _products.add(product);
   }
 
-  Future<Database> _initDB(String filePath) async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
-
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: _createDB,
-    );
-  }
-
-  Future _createDB(Database db, int version) async {
-    // جدول المنتجات
-    await db.execute('''
-      CREATE TABLE products (
-        id TEXT PRIMARY KEY,
-        name TEXT NOT NULL,
-        price REAL NOT NULL,
-        stock_quantity INTEGER NOT NULL,
-        category TEXT NOT NULL
-      )
-    ''');
-  }
-
-  // إضافة منتج
-  Future<int> insertProduct(ProductModel product) async {
-    final db = await instance.database;
-    return await db.insert('products', product.toJson());
-  }
-
-  // جلب كافة المنتجات
   Future<List<ProductModel>> getAllProducts() async {
-    final db = await instance.database;
-    final result = await db.query('products');
-    return result.map((json) => ProductModel.fromJson(json)).toList();
+    return _products;
+  }
+
+  // العملاء
+  Future<void> insertCustomer(CustomerModel customer) async {
+    _customers.add(customer);
+  }
+
+  Future<List<CustomerModel>> getAllCustomers() async {
+    return _customers;
+  }
+
+  // الطلبات
+  Future<void> insertOrder(OrderModel order) async {
+    _orders.add(order);
+  }
+
+  Future<List<OrderModel>> getAllOrders() async {
+    return _orders;
   }
 }
